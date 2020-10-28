27c62dd357231e0d18c7e27af48bba14efa60e4d
Maksim Perevyazkin <kvadratpm@gmail.com>
2020-10-28 10:26:32 +0300

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (Array.isArray(arr)) {
+    return arr
+    .map((elem) => typeof elem === 'string' ? elem.trim().slice(0, 1).toUpperCase() : '').sort().join('');
+  } else {
+    return false;
+  }
 };

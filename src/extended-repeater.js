6695945a90b34e3c8a14a8a24044398b7a3696dd
Maksim Perevyazkin <kvadratpm@gmail.com>
2020-10-29 12:14:08 +0300

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater (str, obj) {
+  let acc = '';
+  const defObj = {repeatTimes: 1, separator: '+', additionRepeatTimes: 0, additionSeparator: '|' };
+  Object.keys(defObj).forEach((elem) => {
+    obj.hasOwnProperty(elem) && obj[elem] ? null : obj[elem] = defObj[elem];
+  });
+  if (obj.repeatTimes) {
+    for (let i = 0; i < obj.repeatTimes; i++) {
+      acc += str;
+      if (obj.additionRepeatTimes > 0) {
+        console.log(acc)
+        for (let j = 0; j < obj.additionRepeatTimes; j++) {
+          acc += obj.addition;
+          j === obj.additionRepeatTimes - 1 ? null : acc += obj.additionSeparator;
+        }
+      } else if (obj.additionRepeatTimes === 0 && obj.addition){
+        acc += obj.addition;
+      }
+      i === obj.repeatTimes - 1 ? null : acc += obj.separator;
+    }
+  }
+  return acc;
 };
+//repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+
+//'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
   
